dcc2ed2cbdf6dccb67ae705c907a9463d0bdfd27
mariavictor <53283315+mariavictor@users.noreply.github.com>
2019-07-24 22:58:27 -0300

    Add files via upload

---
+++ src/compoments/pages/SocialView/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import BoxMain { StyledBoxMain, StyledHeader, StyledPeopleRight, StyledMainCenter ,StyledNoticiasLeft } from '.../.../.../styled'
+
+class SocialView  extends Component {
+    render() {
+      return (
+                    <BoxMain>
+                             fefef
+                    </BoxMain>
+      )
+    }
+  }
+  
+  export default SocialView
+  
